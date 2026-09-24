f604d7be2da9940cdd045be483ff43477401b247
sh7adow85 <sh7adow2007@gmail.com>
2026-09-24 08:55:35 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
 
 caesar = "Graham"
-praline 
+praline = 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
 # print(praline)
