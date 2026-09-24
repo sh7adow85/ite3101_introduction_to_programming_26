343b3a9163dd6e284b9fe8309dbcfd25ada254e9
sh7adow85 <sh7adow2007@gmail.com>
2026-09-24 08:09:20 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -12,5 +12,5 @@ october_rainfall = 7.20
 november_rainfall = 5.06
 december_rainfall = 4.06
 
- annual_rainfall +=semtember_rainball + \
-     october_rainfall + november_rainball + december_rainball
+annual_rainfall += semtember_rainball + \
+    october_rainfall + november_rainball + december_rainball
